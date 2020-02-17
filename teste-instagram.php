b62e93e68c54c74919e29b03f1b2ee77c2db558d
Otiremor <41696444+Otiremor@users.noreply.github.com>
2020-02-17 11:19:44 -0300

    arquivo para testar api do instagram

---
+++ teste-instagram.php
@@ -0,0 +1,5 @@
+https://api.instagram.com/oauth/authorize
+  ?client_id=616450505602373
+  &redirect_uri=//otiremor.github.io/aprendendo-html/
+  &scope=user_profile,user_media
+  &response_type=code
